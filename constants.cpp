d39e3469adad6ac3b36ea5d8e9a533d78770a623
Brooklyne Finni <brooklyne.finni@gmail.com>
2019-07-27 16:10:30 -0400

    Add files via upload

---
+++ constants.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define width 5;
+#define length 10;
+#define newline '\n';
+
+int main(){
+  int area;
+  area = length * width;
+  cout << area;
+  cout << newline;
+
+  return 0;
+
+}
